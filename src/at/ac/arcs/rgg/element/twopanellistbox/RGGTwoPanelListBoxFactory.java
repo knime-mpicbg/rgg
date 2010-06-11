230f4c5345cf89926fa8221ee3a8c1bbfc661cb2
Holger Brandl <watzlawwutz@gmx.net>
2010-06-11 10:49:18 +0200

    added new 2panel-listbox

---
+++ src/at/ac/arcs/rgg/element/twopanellistbox/RGGTwoPanelListBoxFactory.java
@@ -0,0 +1,76 @@
+/*
+ * RGGListFactory.java
+ *
+ * Created on 12. November 2006, 01:16
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package at.ac.arcs.rgg.element.twopanellistbox;
+
+import at.ac.arcs.rgg.RGG;
+import at.ac.arcs.rgg.element.RElement;
+import at.ac.arcs.rgg.factories.RElementFactory;
+import at.ac.arcs.rgg.layout.LayoutInfo;
+import org.apache.commons.lang.StringUtils;
+import org.w3c.dom.Element;
+
+import java.util.Arrays;
+
+
+/**
+ *
+ * @author ilhami
+ */
+public class RGGTwoPanelListBoxFactory extends RElementFactory{
+
+    public RElement createRGGElement(Element element, RGG rggInstance) {
+        if(element.getNodeType() != Element.ELEMENT_NODE)
+            throw new IllegalArgumentException("elements node type must be ELEMENT_NODE");
+
+
+
+        /****************** initialize and set attributes values **************************************/
+        String var = element.getAttribute(RGG.getConfiguration().getString("VAR"));
+        String label = element.getAttribute(RGG.getConfiguration().getString("LABEL"));
+        String colspan = element.getAttribute(RGG.getConfiguration().getString("COLUMN-SPAN"));
+        String items = element.getAttribute(RGG.getConfiguration().getString("ITEMS"));
+        String datatype = element.getAttribute(RGG.getConfiguration().getString("DATA-TYPE"));
+        /***********************************************************************************************/
+        VTwoPanelListBox vList = new VTwoPanelListBox();
+        RTwoPanelListBox rListBox = new RTwoPanelListBox();
+
+        if(StringUtils.isNotBlank(items)){
+            vList.setListData(Arrays.asList(StringUtils.split(items,',')));
+        }
+        rListBox.setVList(vList);
+
+        if(StringUtils.isNotBlank(var)){
+            rListBox.setVar(var);
+        }
+
+        if(StringUtils.isNotBlank(label)){
+            rListBox.setLabel(label);
+        }
+
+        if(StringUtils.isNotBlank(colspan)){
+            if(StringUtils.isNumeric(colspan)){
+                vList.setColumnSpan(Integer.parseInt(colspan));
+            }else if(StringUtils.equals(colspan, RGG.getConfiguration().getString("FULL-SPAN")))
+                vList.setColumnSpan(LayoutInfo.FULL_SPAN);
+            else
+                throw new NumberFormatException(RGG.getConfiguration().getString("COLUMN-SPAN")
+                        +" seems not to be a number: "+
+                        colspan);
+        }
+
+        if(StringUtils.isNotBlank(datatype)){
+            if(StringUtils.equalsIgnoreCase(RGG.getConfiguration().getString("NUMERIC"),datatype))
+                rListBox.setNumeric(true);
+        }
+
+        return rListBox;
+    }
+
+}
---
+++ src/at/ac/arcs/rgg/element/twopanellistbox/VTwoPanelListBox.java
@@ -0,0 +1,112 @@
+/*
+ * VList.java
+ *
+ * Created on 25. Juli 2006, 15:32
+ */
+
+package at.ac.arcs.rgg.element.twopanellistbox;
+
+import at.ac.arcs.rgg.component.VisualComponent;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ *
+ * @author ilhami
+ */
+public class VTwoPanelListBox extends VisualComponent{
+    private JLabel label;
+    private JComponent[][] swingComponents;
+
+    private boolean labelTextSet = false;
+    private TwoPaneSelectionPanel<String> selectPanel;
+
+
+    /**
+     * Creates a new instance of VList
+     */
+    public VTwoPanelListBox() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        label = new JLabel();
+        selectPanel = new TwoPaneSelectionPanel<String>(false, new DefaultListCellRenderer());
+    }
+
+    public void setListData(List<String> items){
+        selectPanel.update(items);
+    }
+
+    public JComponent[][] getSwingComponents() {
+        if(swingComponents == null){
+            if(labelTextSet)
+                swingComponents = new JComponent[][]{{label},{selectPanel}};
+            else
+                swingComponents = new JComponent[][]{{selectPanel}};
+        }
+
+        return swingComponents;
+    }
+
+//    public void setSelectedIndex(int index){
+//        list.setSelectedIndex(index);
+//    }
+//
+//
+//    public void setSelectedIndices(int[] selection) {
+//        list.setSelectedIndices(selection);
+//    }
+//
+//
+//    public int getSelectedIndex() {
+//        return list.getSelectedIndex();
+//    }
+//
+//
+//    public int[] getSelectedIndices() {
+//        return list.getSelectedIndices();
+//    }
+//
+//
+//    public Object getSelectedValue(){
+//        return list.getSelectedValue();
+//    }
+
+    public Object[] getSelectedValues(){
+        return selectPanel.getIncludedColumnSet().toArray();
+    }
+
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public void setLabelText(final String labeltext) {
+        labelTextSet = true;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                label.setText(labeltext);
+            }
+        });
+    }
+
+
+
+    public String getLabelText() {
+        return label.getText();
+    }
+
+    public boolean isVisualComponent() {
+        return true;
+    }
+
+    public void setColumnSpan(int colspan){
+        //todo what is this
+//        if(colspan > 0)
+//            layoutInfo.setComponentColumnSpan(list,colspan);
+    }
+}
